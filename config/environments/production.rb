1ae704e302c98bc17bc210985b2c24c92e5be802
Sofia Sousa <csofiamsousa@gmail.com>
2018-06-19 11:04:36 +0100

    Set default url protocol to https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,7 @@
 
 environment_configuration(defined?(config) && config) do |config|
   # Settings specified here will take precedence over those in config/application.rb
+  config.action_controller.default_url_options = { protocol: 'https' }
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
